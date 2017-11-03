4cf2cf21c480d39c16745d1ac2a289a1d56f5caf
kiran-TT-web <kmekala@traveltripper.com>
2017-11-03 15:25:49 +0530

    navgation silder welcome sections

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -8,4 +8,31 @@ $(document).ready(function() {
 
   // init smooth scroll
   $("a").smoothScroll({ offset: -20 });
+
+  //hero auto image widnow height
+    // function setHeight() {
+    //   windowHeight = $(window).innerHeight();
+    //   $('.hero-slider .carousel-item, .hero-slider .carousel-inner').css('min-height', windowHeight - 113);
+    // }
+    // setHeight();
+    
+    // $(window).resize(function() {
+    //   setHeight();
+    // }); 
+    
+
+  //main nav scroll resize
+  function changeHeader(){
+    if($( window ).width() > 991){
+    if($(window).scrollTop() > 200) {
+      $(".navbar").addClass("shrink-nav"); 
+       $(".navbar").css({'top':'0px','opacity':'1'}); 
+       $('body').css('margin-top','115px');
+    }
+    else {
+      $(".navbar").removeClass("shrink-nav");
+      $('body').css('margin-top','');
+    }
+    }
+  }
 });
