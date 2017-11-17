503f1f7e0070e83b43740a7feec4f52d4ab59bbd
kiran-TT-web <kmekala@traveltripper.com>
2017-11-17 17:31:34 +0530

    gallery page done

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -51,14 +51,12 @@ $(document).ready(function() {
     //tonightRate 
     $('#view_rates').click(function () {
         $("#tongihtrates").toggle('slow');
-
     });
-
     $('#close').click(function (e) {
         e.preventDefault();
         $('#tongihtrates').toggle('hide');
     }); 
-
+    //Attraction slider & more offers and more rooms
     $('.attractions-list,.assets-slide-inner').slick({
       dots: false,
       infinite: true,
@@ -89,10 +87,49 @@ $(document).ready(function() {
       
       ]
     });
-
+    //Attraction information 
     $('.attractions-list').on('afterChange', function(event, slick, currentSlide, nextSlide){
     var $slides = $('.attractions-list').slick("getSlick").$slides;
     var title = $slides.eq( currentSlide ).data("text");
     $('.info').html( title );
   });
+
+  //Gallery filter
+  $(".filter").on("click", function () {
+    var $this = $(this);
+    // if we click the active tab, do nothing
+    if (!$this.hasClass("active")) {
+      $(".filter").removeClass("active");
+      $this.addClass("active"); // set the active tab
+      var $filter = $this.data("rel"); // get the data-rel value from selected tab and set as filter
+      $filter == 'all' ? // if we select "view all", return to initial settings and show all
+        $(".fancybox").attr("data-fancybox-group", "gallery").not(":visible").fadeIn() : // otherwise
+        $(".fancybox").fadeOut(0).filter(function () {
+          return $(this).data("filter") == $filter; // set data-filter value as the data-rel value of selected tab
+        }).attr("data-fancybox-group", $filter).fadeIn(1000); // set data-fancybox-group and show filtered elements
+      //reset lightgallery after filtering
+      setTimeout(function () {
+        var lightgallery = $('#lightgallery');
+        if (lightgallery.length > 0) {
+          lightgallery.data('lightGallery').destroy(true);
+          $('#lightgallery').lightGallery({
+            selector: ".item:visible",
+            counter: false,
+            thumbnail: false,
+            share: false
+
+          });
+        }
+
+      }, 1000);
+    } // if
+  }); // on
+
+  $('#lightgallery').lightGallery({
+    selector: ".item",
+    counter: false,
+    thumbnail: true,
+    share: false
+  });
+
 });
