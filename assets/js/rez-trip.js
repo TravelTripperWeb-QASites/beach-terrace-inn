a208fe0edb431f344ff2de594384ca74ff33d20b
Renu <rgunpal@traveltripper.com>
2018-07-05 23:12:13 +0530

    Revert back hotel credentials to original

--- assets/js/rez-trip.js
+++ assets/js/rez-trip.js
@@ -5,8 +5,8 @@
       $interpolateProvider.endSymbol(']]');
     })
     .value('rt3api', new Rt3Api({
-      portalId: 'hoteldivasf',
-      hotelId: 'SFOHDI',
+      portalId: 'beachterraceinn',
+      hotelId: 'CABTI',
       defaultLocale: 'en',
       defaultCurrency: 'USD'
     }))
